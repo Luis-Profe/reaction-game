bcaa74f55d67c7db257982f150b277bd6e861e54
Luis-Profe <98078028+Luis-Profe@users.noreply.github.com>
2022-05-24 00:24:59 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,60 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # . .
+        # # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(randint(3000, 6000))
+    game_started = true
+    basic.showIcon(IconNames.Heart)
+    inicio = input.runningTime()
+})
 input.onPinPressed(TouchPin.P2, function () {
     if (!(game_started)) {
         basic.showString("A")
         puntA += 1
-        game_started = false
         muestrspuntos()
+    } else {
+        basic.showString("B")
+        puntB += 1
+        muestrspuntos()
+        muestratiempo()
     }
 })
 function muestrspuntos () {
+    game_started = false
     basic.clearScreen()
     basic.pause(500)
-    tiempo = input.runningTime() - inicio
-    basic.showNumber(tiempo / 1000)
-    basic.pause(3000)
+    basic.clearScreen()
+    basic.showString("JugA")
     basic.showNumber(puntA)
-    basic.showString("A")
     basic.pause(1000)
     basic.clearScreen()
+    basic.showString("Jug B")
     basic.showNumber(puntB)
-    basic.showString("B")
     basic.pause(1000)
     basic.clearScreen()
 }
@@ -25,37 +62,25 @@ input.onPinPressed(TouchPin.P1, function () {
     if (!(game_started)) {
         basic.showString("B")
         puntB += 1
-        game_started = false
         muestrspuntos()
+    } else {
+        basic.showString("A")
+        puntA += 1
+        muestrspuntos()
+        muestratiempo()
     }
 })
-let inicio = 0
+function muestratiempo () {
+    basic.clearScreen()
+    tiempo = input.runningTime() - inicio
+    basic.showNumber(Math.idiv(tiempo, 1000))
+    basic.pause(1000)
+    basic.clearScreen()
+}
 let tiempo = 0
+let inicio = 0
 let game_started = false
 let puntB = 0
 let puntA = 0
 puntA = 0
 puntB = 0
-basic.forever(function () {
-    game_started = false
-    basic.pause(randint(1000, 5000))
-    game_started = true
-    basic.showIcon(IconNames.Heart)
-    inicio = input.runningTime()
-    while (game_started) {
-        if (input.pinIsPressed(TouchPin.P1)) {
-            basic.showString("A")
-            puntA += 1
-            game_started = false
-            muestrspuntos()
-        } else {
-            if (input.pinIsPressed(TouchPin.P2)) {
-                basic.showString("B")
-                puntB += 1
-                game_started = false
-                muestrspuntos()
-            }
-        }
-    }
-    basic.clearScreen()
-})
