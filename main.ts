27ec334b860ff4db865e2ab7553cdad554698a9c
Luis-Profe <98078028+Luis-Profe@users.noreply.github.com>
2022-05-23 23:46:24 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,41 @@
-let tiempo = 0
+input.onPinPressed(TouchPin.P2, function () {
+    if (!(game_started)) {
+        basic.showString("A")
+        puntA += 1
+        game_started = false
+        muestrspuntos()
+    }
+})
+function muestrspuntos () {
+    basic.clearScreen()
+    basic.pause(500)
+    tiempo = input.runningTime() - inicio
+    basic.showNumber(tiempo / 1000)
+    basic.pause(3000)
+    basic.showNumber(puntA)
+    basic.showString("A")
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showNumber(puntB)
+    basic.showString("B")
+    basic.pause(1000)
+    basic.clearScreen()
+}
+input.onPinPressed(TouchPin.P1, function () {
+    if (!(game_started)) {
+        basic.showString("B")
+        puntB += 1
+        game_started = false
+        muestrspuntos()
+    }
+})
 let inicio = 0
+let tiempo = 0
 let game_started = false
-let puntA = 0
 let puntB = 0
+let puntA = 0
+puntA = 0
+puntB = 0
 basic.forever(function () {
     game_started = false
     basic.pause(randint(1000, 5000))
@@ -14,24 +47,15 @@ basic.forever(function () {
             basic.showString("A")
             puntA += 1
             game_started = false
+            muestrspuntos()
         } else {
             if (input.pinIsPressed(TouchPin.P2)) {
                 basic.showString("B")
                 puntB += 1
                 game_started = false
+                muestrspuntos()
             }
         }
     }
-    tiempo = input.runningTime() - inicio
-    basic.clearScreen()
-    basic.showNumber(tiempo / 1000)
-    basic.pause(3000)
-    basic.showNumber(puntA)
-    basic.showString("A")
-    basic.pause(1000)
-    basic.clearScreen()
-    basic.showNumber(puntB)
-    basic.showString("B")
-    basic.pause(1000)
     basic.clearScreen()
 })
